9975f0dd03e1a44d676457290766113738242683
brendonwang9 <brendonwang30@gmail.com>
2022-02-09 12:26:12 +1100

    create states

--- src/TextBox.js
+++ src/TextBox.js
@@ -1,10 +1,17 @@
 import './TextBox.css'
 
-export default function TextBox() {
+export default function TextBox({ displayText, setDisplayText }) {
+    var lastText = displayText.slice(-1)
+    function handleChange(event) {
+        var newarr = [...displayText.slice(0, -1), event.target.value]
+        setDisplayText(newarr)
+        console.log(newarr)
+    }
     return (
         <div className='textbox-element'>
-            <textarea></textarea>
+            <textarea value={lastText} onChange={handleChange}>
+            </textarea>
             <button></button>
-        </div>
+        </div >
     )
 }
