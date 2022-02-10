40a9572542b08df33af78a2d3d5c70d6e3b30b8e
HarryP <harrypham__@hotmail.com>
2022-02-10 15:23:15 +1100

    update css/merge text to speech

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import './App.css';
 import TextBox from './TextBox.js';
 import MessageHistory from './MessageHistory.js'
 import Select from "./Select.js"
+// data we need
+// 1. display text => speech to text output
+// 2. output language 
+
+// wireframe 
+// 1. speech to text => string text in english 
+// 2. textbox
+//3. messageHistory <= array ["string 1 ", "string 2"] <= text to text 
+
 
 function App() {
   var [displayText, setDisplayText] = useState([])
@@ -12,18 +21,20 @@ function App() {
   var [currentWord, setCurrentWord] = useState('')
   var [targetLanguage, setTargetLanguage] = useState("Google Bahasa Indonesia")
 
-
   return (
     <div className="App">
-      <header>
-        <h1 className='header'>Trainslator</h1>
-        <Select targetLanguage={targetLanguage} setTargetLanguage={setTargetLanguage} />
-      </header>
-      <section>
-        <MessageHistory displayText={displayText} translated={translated} />
-      </section>
-      <TextBox displayText={displayText} setDisplayText={setDisplayText} translated={translated} setTranslated={setTranslated} currentWord={currentWord} setCurrentWord={setCurrentWord} targetLanguage={targetLanguage} setTargetLanguage={setTargetLanguage} />
-      <SpeechToText displayText={displayText} setDisplayText={setDisplayText} currentWord={currentWord} setCurrentWord={setCurrentWord} />
+
+      <div className='container'>
+        <header>
+          <h1 className='header'>Trainslator</h1>
+        </header>
+        <section className='message-container'>
+          <MessageHistory displayText={displayText} translated={translated}/>
+        </section>
+          <TextBox displayText={displayText} setDisplayText={setDisplayText} translated={translated} setTranslated={setTranslated} currentWord={currentWord} setCurrentWord={setCurrentWord} targetLanguage={targetLanguage} setTargetLanguage={setTargetLanguage} />
+          <SpeechToText displayText={displayText} setDisplayText={setDisplayText} currentWord={currentWord} setCurrentWord={setCurrentWord}/>
+          <Select targetLanguage={targetLanguage} setTargetLanguage={setTargetLanguage} />
+      </div>
     </div>
   )
 }
--- src/Select.js
+++ src/Select.js
@@ -1,17 +1,22 @@
+import { useEffect } from 'react'
+
 export default function Select({ targetLanguage, setTargetLanguage }) {
     var synth = window.speechSynthesis.getVoices()
     var voices = []
+
     for (let i = 0; i < synth.length; i++) {
         if (synth[i].voiceURI.slice(0, 6) === "Google") {
             voices.push(synth[i])
         }
     }
+
     voices.sort(function (a, b) {
         const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
         if (aname < bname) return -1;
         else if (aname == bname) return 0;
         else return +1;
     });
+
     function Options() {
         var Options = []
         for (let i = 0; i < voices.length; i++) {
@@ -23,10 +28,12 @@ export default function Select({ targetLanguage, setTargetLanguage }) {
         }
         return Options
     }
+
     function handleChange(event) {
         var newOption = event.target.value
         setTargetLanguage(newOption)
     }
+    
     return (
         <select value={targetLanguage} onChange={handleChange}>
             <Options />
