a7f3706772538a3e76dee8f2f612a1b244f702a0
brendonwang9 <brendonwang30@gmail.com>
2022-02-11 11:45:26 +1100

    refactoring and googletags

--- src/App.js
+++ src/App.js
@@ -21,10 +21,18 @@ function App() {
   var [displayText, setDisplayText] = useState([])
   var [translated, setTranslated] = useState([])
   var [currentWord, setCurrentWord] = useState('')
-  var [originalLanguage, setOriginalLanguage] = useState("Google US English")
-  var [targetLanguage, setTargetLanguage] = useState("Google 한국의")
+  var [originalLanguage, setOriginalLanguage] = useState("US English")
+  var [targetLanguage, setTargetLanguage] = useState("한국의")
   var [isToggled, setIsToggled] = useState(false)
 
+  function P(props) {
+    console.log(props.classname)
+    return (
+      <p className={props.classname}>
+        hello
+      </p>
+    )
+  }
   return (
     <div className="App">
 
@@ -41,7 +49,7 @@ function App() {
           <div className='arrow right'></div>
           <SelectTarget targetLanguage={targetLanguage} setTargetLanguage={setTargetLanguage} />
         </div>
-        <SpeechToText displayText={displayText} setDisplayText={setDisplayText} currentWord={currentWord} setCurrentWord={setCurrentWord} originalLanguage={originalLanguage} />
+        <SpeechToText setCurrentWord={setCurrentWord} originalLanguage={originalLanguage} />
         <Toggle isToggled={isToggled} onToggle={() => setIsToggled(!isToggled)} />
       </div>
     </div>
